e393cedb28a849382bbc85030ddda4b5f3dad051
Kevin <chatelainkevin9@gmail.com>
2022-11-27 20:59:08 +0100

    Design du menu

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -1,4 +1,4 @@
-function showPokemon(data, pokemonAmount, startingId) {
+async function showPokemon(data, pokemonAmount, startingId) {
   for(let i=startingId; i<pokemonAmount; i++) {
       document.querySelector('.pokemon-list')
       .innerHTML += `<div class="pokemon-container" id="${data[i].id}">
@@ -28,11 +28,11 @@ fetch('https://pokebuildapi.fr/api/v1/pokemon')
 })
 .then(function(json) {     
   console.log(json)  
-  showPokemon(json, 50, 0);
+  showPokemon(json, 10, 0);
 
   let showPokemonAmount = 100;
 
-  document.querySelector('button').addEventListener('click', () => {
+  document.querySelector('.showmore-button').addEventListener('click', () => {
     let lastPokemon = document.querySelector('.pokemon-container:last-child').getAttribute('id');
     showPokemon(json, (parseInt(lastPokemon) + showPokemonAmount), lastPokemon);
   })
