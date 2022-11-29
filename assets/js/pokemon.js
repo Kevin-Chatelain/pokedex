89dd967910e7625bb45a3f538ea4145470be9cc3
Kevin <chatelainkevin9@gmail.com>
2022-11-29 23:40:51 +0100

    Début des fiches de chaque pokemon

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -21,6 +21,16 @@ async function showPokemon(data, pokemonAmount, startingId) {
      }
 }
 
+async function printPokemonData(data, pokemonId) {
+  document.querySelector('.fiche-infos-nom h1').innerText = data[pokemonId].name;
+  document.querySelector('.fiche-infos-id p').innerText = data[pokemonId].id;
+
+  let typesArray = data[pokemonId].apiTypes;
+  typesArray.forEach(types => {
+    document.querySelector('.fiche-infos-types').innerHTML += `<img src="${types.image}" alt="type-${types.name}" />`;
+  });
+}
+
 
 fetch('https://pokebuildapi.fr/api/v1/pokemon')
 .then(function(response) {  
@@ -35,7 +45,16 @@ fetch('https://pokebuildapi.fr/api/v1/pokemon')
   document.querySelector('.showmore-button').addEventListener('click', () => {
     let lastPokemon = document.querySelector('.pokemon-container:last-child').getAttribute('id');
     showPokemon(json, (parseInt(lastPokemon) + showPokemonAmount), lastPokemon);
-  })
+  });
+
+  document.querySelectorAll('.pokemon-container').forEach(pokemonContainer => {
+    pokemonContainer.addEventListener('click', function() {
+      let pokemonId = parseInt(this.getAttribute('id')) - 1;
+      document.querySelector('.fiche-infos-types').innerHTML = "";
+      printPokemonData(json, pokemonId)
+    })
+  });
+
 });
 
 
